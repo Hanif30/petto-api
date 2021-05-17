d71d182dafd1158992416a427139ffef341ff50b
Hanif <mh.hanif30@gmail.com>
2021-05-17 15:48:52 +0800

    add mongo db connection

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM node:16-alpine3.11
 
 WORKDIR /usr/src/app
 
+COPY ./package*.json ./
+
 RUN npm install
 
 COPY . .
--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
 
+const db = require('./db')
+
 const app = express()
 const port = 5000
 
 // parse request bodies (req.body)
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.get('/', (req, res) => {
   res.send('Welcome to Petto API!')
